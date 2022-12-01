cf6d5da55c322c92b5b6ece76178f059157a25ea
ismam3 <ismamlabib3@gmail.com>
2022-12-01 19:08:08 +0600

    Initial commit

---
+++ scripts/js/loader.js
@@ -0,0 +1,30 @@
+import * as THREE from "./three/build/three.module.js";
+import {GLTFLoader} from "./three/examples/jsm/loaders/GLTFLoader.js";
+
+
+let loader = new GLTFLoader();
+const texture = new THREE.TextureLoader().load( "../models/Textures/environment.jpg" );
+texture.mapping = THREE.EquirectangularReflectionMapping;
+loader.load(
+    "../exteriorViz1/models/model.glb",
+    (gltf)=>{
+        var scene = document.querySelector('a-scene').object3D;
+        var model = gltf.scene;
+        model.position.set(0, 0.033, -16.5);
+        scene.add( gltf.scene );
+        scene.environment = texture;
+        console.log(scene)
+    },
+    (model)=>{
+        let fraction = model.loaded/model.total*100;
+        let percentage = Math.trunc(fraction);
+        document.getElementById("status").innerHTML = "Assets are loading!<br>"+percentage+"% loaded &#128571;";
+        document.getElementById("progress").style.width = percentage+"%";
+        if(percentage===100||percentage>100){
+            document.getElementById("status").innerHTML = "Assets are loaded!&#129409; Your browser is rendering<br>Please wait a FEW SECONDS!";
+            setTimeout(()=>{
+                document.getElementById("preloader").style.display="none";
+            },5000)
+        }
+    }
+)
