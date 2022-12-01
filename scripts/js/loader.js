853cd2358ae65b32bbf0d6084a9f6e219acab186
ismam3 <ismamlabib3@gmail.com>
2022-12-01 20:06:48 +0600

    trees path is fixed

--- scripts/js/loader.js
+++ scripts/js/loader.js
@@ -18,16 +18,19 @@ loader.load(
         let fraction = model.loaded/model.total*100;
         let percentage = Math.trunc(fraction);
         document.getElementById("status").innerHTML = "Assets are loading!<br>"+percentage+"% loaded &#128571;";
-        document.getElementById("progress").style.width = percentage+"%";
+        
         if(percentage===100||percentage>100){
             document.getElementById("status").innerHTML = "Assets are loaded!&#129409; Your browser is rendering<br>Please wait a FEW SECONDS!";
             setTimeout(()=>{
                 document.getElementById("preloader").style.display="none";
             },5000)
         }
+        else{
+            document.getElementById("progress").style.width = percentage+"%";
+        }
     }
 )
-loader.load("../exteriorViz1/models/tree.glb",(gltf)=>{
+loader.load("../exteriorViz1/models/trees.glb",(gltf)=>{
     var scene = document.querySelector('a-scene').object3D;
         var model = gltf.scene;
         model.position.set(0, 0, -15.26);
